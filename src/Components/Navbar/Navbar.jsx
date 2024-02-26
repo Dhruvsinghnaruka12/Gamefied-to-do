c56b243d7b358321a0deb456245591e13f9c1a9e
Rekha Naruka <dhruvsinghnaruka12@gmail.com>
2024-02-26 21:04:48 +0530

    removal of search and night mode

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import logo_light from'../../assets/logo-black.png'
 import logo_dark from'../../assets/logo-white.png'
-import search_icon_light from'../../assets/search-w.png'
-import search_icon_dark from'../../assets/search-b.png'
-import toggle_dark from'../../assets/day.png'
-import toggle_light from'../../assets/night.png'
+
 
 
 const Navbar = ({theme,setTheme}) => {
@@ -19,14 +16,11 @@ const Navbar = ({theme,setTheme}) => {
         <ul>
             <li>Home</li>
             <li>About</li>
-            <li>Login</li>
-            <li>Signup</li>
+            <li>Testimonial</li>
+            <li>Register</li>
         </ul>
-    <div className='search-box'>
-        <input type='text' placeholder='Search'/>
-        <img src={ theme=='light'? search_icon_light:search_icon_dark} alt="" />
-    </div>
-    <img  onClick ={()=>{toggle_mode()}} src={ theme=='light'?toggle_light:toggle_dark} alt="" className='toggle-icon'/>
+    
+   
     </div>
   )
 }
